79180817c23dae5e19956f82307411053bfa9133
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2022-07-01 22:25:05 +0200

    Comments

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
+        
     <?php
+    // Single Line Comment 
 
         echo "Hello From Head";
+
+    /*
+        Multi Line Comment
+
+    */
 
     ?>
     </title>
