348be054c27034e6ca94b1292154053532db7f7d
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2022-07-01 23:39:49 +0200

    Variables

---
+++ var.php
@@ -0,0 +1,13 @@
+<?php
+
+$x = 5; //integer
+$api = 3.14; //float
+$name = "Muhammed Salama"; //string
+$is_admin = true; //boolean
+$phone = null; //null
+
+echo $api . ' ' . $x;
+//gettype
+echo gettype($x);
+
+?>
